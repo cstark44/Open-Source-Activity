9fea6a7a82944ceaf0d6356617fa936142b2b85f
cstark44 <112347677+cstark44@users.noreply.github.com>
2023-03-21 10:19:46 -0400

    Added some comments

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,6 +1,9 @@
+# Open File
 with open("numbers.html", "w") as f:
+    # Write html code
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
     f.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n")
+    # Make a table
     for i in range(1, 50):
         if i % 2 == 0:
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
