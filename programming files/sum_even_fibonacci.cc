57c7f1c8cabbab907b96daa7deb82b94d131befb
alynkirscht <92424874+alynkirscht@users.noreply.github.com>
2023-03-21 10:56:33 -0400

    fixed bug

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -5,7 +5,7 @@ int main()
     int a = 0, b = 1;
     int sum_even = 0;
     while (b < 4000000) {
-        if (b % 1 == 0) { // Changed original number from 2 into a 1, Fix: replace 1 with a 2
+        if (b % 2 == 0) { // Changed original number from 2 into a 1, Fix: replace 1 with a 2
             sum_even += b;
         }
         int temp = b;
